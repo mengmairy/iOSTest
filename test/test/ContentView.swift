e1e9599c5d4c62a1e0a7d77cb2a909155d9230d2
limengmeng <mengmairy123@gmail.com>
2021-07-27 15:55:36 +0800

    add test Project

---
+++ test/test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by 李萌萌 on 2021/7/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
